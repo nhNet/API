f525669bef67826f60a27a5778f3dbbcac60442b
Browsers <38656592+nhNet@users.noreply.github.com>
2018-11-16 07:41:25 +0100

    Create urlEncode.js

---
+++ urlEncode.js
@@ -0,0 +1,22 @@
+function checkurl(params,whattodo,iferror){
+  try{
+      var variable=window.location.toString();
+      variable=variable.split('?');
+      variable.shift();
+      try{
+          variable=variable[0].split('&').join(',');
+      }catch(err){
+          //
+      }
+      variable=variable.split('=').join(',');
+      variable=variable.split(',');
+      for(l=0;l<params.length;l++){
+        if(variable.includes(params[l])==true){
+          eval(whattodo[l]);
+        }
+      }
+  }catch(err){
+    //Execute as normal
+    eval(iferror)
+  }
+}
